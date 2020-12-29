efee93fbd8b6a205748c1cf2cea5f25adbb28f6e
AntonBuchynskyi <skivmagg@gmail.com>
2020-12-29 23:43:40 +0200

    Hooks useeffect unmount

--- src/components/Hooks.js
+++ src/components/Hooks.js
@@ -1,78 +1,34 @@
 import React, {memo, useEffect, useMemo, useReducer, useState} from "react";
 
+const Test = () => {
 
-const reducer = (state, action) => {
-    switch (action.type){
-        case "SET_TODO": {
-            return action.payload
-        }
-        case "CHANGE_TODO_STATUS": {
-            return {
-                ...state,
-                completed: !state.completed,
-            };
-        }
-        case "CHANGE_TODO_TITLE":{
-            return {
-                ...state,
-                title: state.title + '!'
-            };
-        }
-        default : {
-            return state;
-        }
-    }
+    useEffect(()=>{
+        console.log('mount');
 
+        return () =>{
+            console.log('unmount');
+        }
+
+    },[])
+
+    return <h2>Lorem ipsum dolor sit amet.</h2>
 }
-const initialState = {
-    userId: null,
-    id: null,
-    title: "",
-    completed: false
-}
+
 
 export default function Hooks() {
-    const [state, dispatch] = useReducer(reducer, initialState);
-    const [userNumber, setNumber] = useState(1);
 
-    useEffect(() => {
-        console.log('Hello');
-        fetch(`https://jsonplaceholder.typicode.com/todos/${userNumber}`)
-            .then(value => value.json())
-            .then(value => {
-                dispatch({type:'SET_TODO', payload: value})
-            });
-
-    }, [userNumber])
-
-    let plusEvent = () => {
-        setNumber((prevState) => prevState < 10 ? prevState + 1 : prevState);
-    }
-    let minusEvent = () => {
-        setNumber((prevState) => prevState > 1 ? prevState - 1 : prevState);
-    }
-    const changeStatus = () =>{
-        dispatch({type:'CHANGE_TODO_STATUS'})
-    }
-const changeTitle = () => {
-        dispatch({type:"CHANGE_TODO_TITLE"})
-}
+    const [isVisible, setIsVisible] = useState(false)
+    const [arr, setArr] = useState([40,53,22,555,343,22]);
+const totalPrice = useMemo(()=>{
+    return arr.reduce((acc,el) => (acc+=el),0)
+},[arr])
     return (
         <div>
-            <button onClick={minusEvent}>- age</button>
-            <button onClick={plusEvent}>+ age</button>
-            <button onClick={changeStatus}>change status</button>
-            <button onClick={changeTitle}>change Title</button>
+            {totalPrice}
             <br/>
-            <h1>User number {userNumber}</h1>
-            <hr/>
-<>
-            {!!state &&
-            (<div>
-                <p>{state.id} - {state.title} - {state.completed.toString()} </p>
-            </div>)
-            }
-</>
+            <button onClick={() => setIsVisible(!isVisible)}>Toogle</button>
+            <button onClick={() => setArr([...arr, 50])}>addPrice</button>
+            {isVisible && <Test/>}
 
         </div>
     );
