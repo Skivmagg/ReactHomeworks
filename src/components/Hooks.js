12ecb21c037dec88d1121ea0a2054c42abd26df9
AntonBuchynskyi <skivmagg@gmail.com>
2020-12-29 18:33:19 +0200

    Hooks reducer lection 1 done

--- src/components/Hooks.js
+++ src/components/Hooks.js
@@ -1,16 +1,47 @@
 import React, {memo, useEffect, useMemo, useReducer, useState} from "react";
 
+
+const reducer = (state, action) => {
+    switch (action.type){
+        case "SET_TODO": {
+            return action.payload
+        }
+        case "CHANGE_TODO_STATUS": {
+            return {
+                ...state,
+                completed: !state.completed,
+            };
+        }
+        case "CHANGE_TODO_TITLE":{
+            return {
+                ...state,
+                title: state.title + '!'
+            };
+        }
+        default : {
+            return state;
+        }
+    }
+
+}
+const initialState = {
+    userId: null,
+    id: null,
+    title: "",
+    completed: false
+}
+
 export default function Hooks() {
-
+    const [state, dispatch] = useReducer(reducer, initialState);
     const [userNumber, setNumber] = useState(1);
-    const [user, setUser] = useState();
-
 
     useEffect(() => {
         console.log('Hello');
-        fetch(`https://jsonplaceholder.typicode.com/users/${userNumber}`)
+        fetch(`https://jsonplaceholder.typicode.com/todos/${userNumber}`)
             .then(value => value.json())
-            .then(value => setUser(value))
+            .then(value => {
+                dispatch({type:'SET_TODO', payload: value})
+            });
 
     }, [userNumber])
 
@@ -20,21 +51,28 @@ export default function Hooks() {
     let minusEvent = () => {
         setNumber((prevState) => prevState > 1 ? prevState - 1 : prevState);
     }
-
+    const changeStatus = () =>{
+        dispatch({type:'CHANGE_TODO_STATUS'})
+    }
+const changeTitle = () => {
+        dispatch({type:"CHANGE_TODO_TITLE"})
+}
     return (
         <div>
             <button onClick={minusEvent}>- age</button>
             <button onClick={plusEvent}>+ age</button>
+            <button onClick={changeStatus}>change status</button>
+            <button onClick={changeTitle}>change Title</button>
             <br/>
             <h1>User number {userNumber}</h1>
             <hr/>
-
-            {!!user &&
+<>
+            {!!state &&
             (<div>
-                <p>{user.id} - {user.name} - {user.email} </p>
+                <p>{state.id} - {state.title} - {state.completed.toString()} </p>
             </div>)
             }
-
+</>
 
         </div>
     );
